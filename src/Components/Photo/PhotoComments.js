607e73852d909dd17faa656c491dddf7bbef3cd8
Joao Vitor Pagani <joaovitorpagani7@hotmail.com>
2024-02-26 21:21:15 -0300

    :lipstick: feat: estilizando o commentario

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { UserContext } from '../../UseContext';
-import PhotoCommentsForm from './PhotoCommentsForm';
-import styles from './PhotoComments.module.css';
+import React from "react";
+import { UserContext } from "../../UseContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
+import styles from "./PhotoComments.module.css";
 
 const PhotoComments = (props) => {
   const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef(null);
   const { login } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
 
   return (
     <>
-      <ul className={styles.comments}>
+      <u ref={commentsSection} className={styles.comments}>
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
             <b>{comment.comment_author}: </b>
             <span>{comment.comment_content}</span>
           </li>
         ))}
-      </ul>
+      </u>
       {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
     </>
   );
