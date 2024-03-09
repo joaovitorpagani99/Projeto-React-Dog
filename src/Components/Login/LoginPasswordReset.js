a04d95cae2645e2e04c05e7a736da5529c2b5354
Joao Vitor Pagani <joaovitorpagani7@hotmail.com>
2024-03-09 16:02:23 -0300

    :recycle: refactor: mudando algumas divs para sections

--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -36,7 +36,7 @@ const LoginPasswordReset = () => {
   }
 
   return (
-    <div>
+    <section className="animeLeft">
       <h1 className="title">Resete a senha</h1>
       <form onSubmit={handleSubmit}>
         <Input
@@ -52,7 +52,7 @@ const LoginPasswordReset = () => {
         )}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   );
 };
 
